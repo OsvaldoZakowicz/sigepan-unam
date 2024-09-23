063480ebf27c66526e45e5d331ec0367d8a05cff
Osvaldo <osvaldozakowicz@gmail.com>
2024-09-23 15:56:28 -0300

    cambia layout base para incluir estilos y script de livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+  {{-- livewire --}}
+  @livewireStyles()
   <!-- Scripts -->
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -36,6 +38,8 @@
       {{ $slot }}
     </main>
   </div>
+  {{-- livewire --}}
+  @livewireScripts()
 </body>
 
 </html>
