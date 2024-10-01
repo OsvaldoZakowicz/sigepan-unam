65af50f47687cb5934e1c453d87611a645a50f73
Osvaldo <osvaldozakowicz@gmail.com>
2024-10-01 16:12:42 -0300

    agrega cambios al modulo de usuarios, estilos a reset password y verify email

--- resources/views/livewire/pages/auth/reset-password.blade.php
+++ resources/views/livewire/pages/auth/reset-password.blade.php
@@ -70,36 +70,35 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
-    <form wire:submit="resetPassword">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+  <form wire:submit="resetPassword">
+    <!-- Email Address -->
+    <div>
+      <x-input-label for="email" :value="__('Email')" />
+      <x-input-error :messages="$errors->get('email')" class="mt-2" />
+      <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <!-- Password -->
+    <div class="mt-4">
+      <x-input-label for="password" :value="__('Password')" />
+      <x-input-error :messages="$errors->get('password')" class="mt-2" />
+      <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+    </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+    <!-- Confirm Password -->
+    <div class="mt-4">
+      <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+      <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+      <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+                    type="password"
+                    name="password_confirmation" required autocomplete="new-password" />
 
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                          type="password"
-                          name="password_confirmation" required autocomplete="new-password" />
+    </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
+    <div class="flex items-center justify-end mt-4">
+      <button type="submit" class="flex justify-center items-center box-border w-fit h-6 m-1 p-1 border-solid border rounded border-blue-600 bg-blue-600 text-center text-neutral-100 uppercase text-xs">
+        {{ __('Reset Password') }}
+      </button>
+    </div>
+  </form>
 </div>
--- resources/views/livewire/pages/auth/verify-email.blade.php
+++ resources/views/livewire/pages/auth/verify-email.blade.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
-{
+new #[Layout('layouts.guest')] class extends Component {
     /**
      * Send an email verification notification to the user.
      */
@@ -36,23 +35,24 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+  <div class="mb-4 text-sm text-neutral-600">
+    {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+  </div>
+
+  @if (session('status') == 'verification-link-sent')
+    <div class="mb-4 font-medium text-sm text-green-600">
+      {{ __('A new verification link has been sent to the email address you provided during registration.') }}
     </div>
+  @endif
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
+  <div class="mt-4 flex items-center justify-between">
+    <button wire:click="sendVerification" type="submit" class="flex justify-center items-center box-border w-fit h-6 m-1 p-1 border-solid border rounded border-blue-600 bg-blue-600 text-center text-neutral-100 uppercase text-xs">
+      {{ __('Resend Verification Email') }}
+    </button>
 
-    <div class="mt-4 flex items-center justify-between">
-        <x-primary-button wire:click="sendVerification">
-            {{ __('Resend Verification Email') }}
-        </x-primary-button>
-
-        <button wire:click="logout" type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            {{ __('Log Out') }}
-        </button>
-    </div>
+    <button wire:click="logout" type="submit"
+      class="underline text-sm text-neutral-600 hover:text-neutral-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      {{ __('Log Out') }}
+    </button>
+  </div>
 </div>
