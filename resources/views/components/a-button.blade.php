69444cfd0dd0344b8f7e51decdf0043d081fa89d
Osvaldo <osvaldozakowicz@gmail.com>
2024-10-04 23:38:16 -0300

    agrega componentes de boton y enlace

---
+++ resources/views/components/a-button.blade.php
@@ -0,0 +1,16 @@
+@props([
+  'route_to',
+  'bg_color' => 'blue-600',
+  'border_color' => 'blue-600',
+  'text_color' => 'neutral-100'
+])
+
+<a {{ $attributes->merge([
+  'class' => 'flex justify-center items-center box-border w-fit h-6 m-2 p-1 border-solid border rounded border-'.$border_color.' bg-'.$bg_color.' text-center text-'.$text_color.' uppercase text-xs',
+]) }}>
+  @if ($slot->isEmpty())
+    <span>enlace</span>
+  @else
+    <span>{{ $slot }}</span>
+  @endif
+</a>
---
+++ resources/views/components/btn-button.blade.php
@@ -0,0 +1,13 @@
+@props([
+  'color' => 'emerald',
+  'btn_type' => 'submit'
+])
+
+<button {{ $attributes->merge([
+  'type' => $btn_type,
+  'class' => 'flex justify-center items-center box-border w-fit h-6 m-2 p-1 border-solid border rounded border-'.$color.'-600 bg-'.$color.'-600 text-center text-neutral-100 uppercase text-xs'
+]) }}>
+  <div class="inline-flex justify-center items-center gap-2">
+    {{ $slot }}
+  </div>
+</button>
