20953fa95d9bb89872915eff79f273a3b58e9c00
Osvaldo <osvaldozakowicz@gmail.com>
2024-11-16 14:10:29 -0300

    agrega cambios al modulo de proveedores
    implementa filtros en la lista de precios por proveedor, implementa el borrado de precios de suministro por proveedor

--- app/Livewire/Suppliers/PriceList.php
+++ app/Livewire/Suppliers/PriceList.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire\Suppliers;
 
+use App\Models\Provision;
 use App\Models\Supplier;
+use App\Models\ProvisionTrademark;
+use App\Models\ProvisionType;
 use Livewire\WithPagination;
 use Livewire\Attributes\Url;
 use Livewire\Component;
@@ -13,23 +16,38 @@ class PriceList extends Component
 
   #[Url]
   public $search = '';
+  #[Url]
+  public $trademark_filter = '';
+  #[Url]
+  public $type_filter = '';
 
   // proveedor al que se le dara una lista de precios
   public $supplier;
+  // marcas
+  public $trademarks;
+  // tipos de suministros
+  public $provision_types;
 
   //* montar datos
   public function mount($id)
   {
     $this->supplier = Supplier::findOrFail($id);
+    $this->trademarks = ProvisionTrademark::orderBy('id', 'desc')->get();
+    $this->provision_types = ProvisionType::all();
   }
 
   //* buscar suministros con precio del proveedor
-  // todo: filtrar por precio, marca, tipo
   public function searchSupplierProvisions()
   {
     return $this->supplier->provisions()
       ->when($this->search, function ($query) {
         $query->where('provision_name', 'like', '%' . $this->search . '%');
+      })
+      ->when($this->trademark_filter, function ($query) {
+        $query->where('provision_trademark_id', $this->trademark_filter);
+      })
+      ->when($this->type_filter, function ($query) {
+        $query->where('provision_type_id', $this->type_filter);
       })
       ->orderBy('id', 'desc')
       ->paginate(10);
@@ -41,10 +59,37 @@ class PriceList extends Component
     $this->resetPage();
   }
 
+  //* eliminar precio del suministro
+  // quita la asociacion suministro con proveedor y el precio,
+  // no borra el suministro.
+  public function delete($id)
+  {
+    try {
+
+      $this->supplier->provisions()
+        ->where('provision_id', $id)->first()->pivot->delete();
+
+      $this->dispatch('toast-event', toast_data: [
+        'event_type'  =>  'success',
+        'title_toast' =>  toastTitle('exitosa'),
+        'descr_toast' =>  toastSuccessBody('precio del suministro', 'eliminado'),
+      ]);
+
+    } catch (\Exception $e) {
+
+      $this->dispatch('toast-event', toast_data: [
+        'event_type'  =>  'error',
+        'title_toast' =>  toastTitle('fallida'),
+        'descr_toast' =>  'error: ' . $e->getMessage() . ' contacte al Administrador',
+      ]);
+
+    }
+  }
+
   public function render()
   {
-    $provisions = $this->searchSupplierProvisions();
+    $provisions_with_price = $this->searchSupplierProvisions();
 
-    return view('livewire.suppliers.price-list', compact('provisions'));
+    return view('livewire.suppliers.price-list', compact('provisions_with_price'));
   }
 }
--- resources/views/livewire/suppliers/price-list.blade.php
+++ resources/views/livewire/suppliers/price-list.blade.php
@@ -6,7 +6,7 @@
     <x-title-section title="lista de precios del proveedor: {{ $supplier->company_name }}">
       <div class="flex gap-2">
         <x-a-button wire:navigate href="{{ route('suppliers-suppliers-index', $supplier->id) }}" bg_color="neutral-100" border_color="neutral-300" text_color="neutral-600">volver</x-a-button>
-        <x-a-button wire:navigate href="{{ route('suppliers-suppliers-price-create', $supplier->id) }}">agregar suministros</x-a-button>
+        <x-a-button wire:navigate href="{{ route('suppliers-suppliers-price-create', $supplier->id) }}">agregar precios</x-a-button>
       </div>
     </x-title-section>
 
@@ -20,23 +20,23 @@
           {{-- termino de busqueda --}}
           <input type="text" wire:model.live="search" name="search" wire:click="resetPagination" placeholder="ingrese un id, o termino de busqueda" class="w-1/4 shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300">
           {{-- filtro de marca --}}
-          {{-- <select name="trademark_filter" wire:model.live="trademark_filter" wire:click="resetPagination()" id="trademark_filter" class="w-1/4 shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300">
+          <select name="trademark_filter" wire:model.live="trademark_filter" wire:click="resetPagination" id="trademark_filter" class="w-1/4 shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300">
             <option value="">seleccione una marca ...</option>
             @forelse ($trademarks as $trademark)
               <option value="{{ $trademark->id }}">{{ $trademark->provision_trademark_name }}</option>
             @empty
               <option value="">sin marcas ...</option>
             @endforelse
-          </select> --}}
+          </select>
           {{-- filtro de tipo --}}
-          {{-- <select name="type_filter" wire:model.live="type_filter" wire:click="resetPagination()" id="type_filter" class="w-1/4 shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300">
+          <select name="type_filter" wire:model.live="type_filter" wire:click="resetPagination" id="type_filter" class="w-1/4 shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300">
             <option value="">seleccione un tipo ...</option>
             @forelse ($provision_types as $type)
               <option value="{{ $type->id }}">{{ $type->provision_type_name }}</option>
             @empty
               <option value="">sin tipos ...</option>
             @endforelse
-          </select> --}}
+          </select>
         </form>
       </x-slot:header>
 
@@ -54,30 +54,30 @@
             </tr>
           </x-slot:tablehead>
           <x-slot:tablebody>
-            @forelse ($provisions as $provision)
-              <tr class="border" wire:key="{{ $provision->id }}">
-                <x-table-td>{{ $provision->id }}</x-table-td>
-                <x-table-td>{{ $provision->provision_name }}</x-table-td>
-                <x-table-td>{{ $provision->trademark->provision_trademark_name }}</x-table-td>
-                <x-table-td>{{ $provision->type->provision_type_name }}</x-table-td>
-                <x-table-td>{{ $provision->provision_quantity }}({{ $provision->measure->measure_abrv }})</x-table-td>
-                <x-table-td>$&nbsp;{{ $provision->pivot->price }}</x-table-td>
+            @forelse ($provisions_with_price as $pwp)
+              <tr class="border" wire:key="{{ $pwp->id }}">
+                <x-table-td>{{ $pwp->id }}</x-table-td>
+                <x-table-td>{{ $pwp->provision_name }}</x-table-td>
+                <x-table-td>{{ $pwp->trademark->provision_trademark_name }}</x-table-td>
+                <x-table-td>{{ $pwp->type->provision_type_name }}</x-table-td>
+                <x-table-td>{{ $pwp->provision_quantity }}({{ $pwp->measure->measure_abrv }})</x-table-td>
+                <x-table-td>$&nbsp;{{ $pwp->pivot->price }}</x-table-td>
                 <x-table-td>
-                  acciones
+                  <x-btn-button type="button" wire:navigate wire:click="delete({{ $pwp->id }})" wire:confirm="¿Desea borrar el registro?, eliminará el precio del proveedor: {{ $supplier->company_name }}, para el suministro: {{ $pwp->provision_name }} {{ $pwp->trademark->provision_trademark_name }} de {{ $pwp->provision_quantity }}({{ $pwp->measure->measure_abrv }})" color="red">eliminar</x-btn-button>
                 </x-table-td>
               </tr>
             @empty
               <tr class="border">
-                <td colspan="3">¡lista vacia!, búsque y agregue suministros a esta lista para comenzar.</td>
+                <td colspan="3">sin registros!</td>
               </tr>
             @endforelse
           </x-slot:tablebody>
         </x-table-base>
       </x-slot:content>
 
-      <x-slot:footer class="hidden py-2">
+      <x-slot:footer class="py-2">
         {{-- paginacion --}}
-        {{ $provisions->links() }}
+        {{ $provisions_with_price->links() }}
         <!-- grupo de botones -->
         <div class="flex"></div>
       </x-slot:footer>
