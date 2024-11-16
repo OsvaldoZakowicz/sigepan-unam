029f24c651c4a8edb9435b9b1fc3fb9ae5f7735f
Osvaldo <osvaldozakowicz@gmail.com>
2024-11-15 22:31:12 -0300

    agrega cambios al modulo de proveedores
    finaliza el cuadro de busqueda de suministros en el alta de precios para proveedor

--- app/Livewire/Suppliers/SearchProvision.php
+++ app/Livewire/Suppliers/SearchProvision.php
@@ -3,7 +3,8 @@
 namespace App\Livewire\Suppliers;
 
 use App\Models\Provision;
-use App\Models\Supplier;
+use App\Models\ProvisionTrademark;
+use App\Models\ProvisionType;
 use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 use Livewire\Component;
@@ -14,14 +15,30 @@ class SearchProvision extends Component
 
   #[Url]
   public $search = '';
+  #[Url]
+  public $search_tr = '';
+  #[Url]
+  public $search_ty = '';
 
   // proveedor
   public $supplier_id;
+  // marcas de suministros
+  public $trademarks;
+  // tipos de suministros
+  public $provision_types;
 
   // montar datos
   public function mount($supplier_id)
   {
     $this->supplier_id = $supplier_id;
+    $this->trademarks = ProvisionTrademark::all();
+    $this->provision_types = ProvisionType::all();
+  }
+
+  // reiniciar la paginacion al buscar
+  public function resetPagination()
+  {
+    $this->resetPage();
   }
 
   //* enviar la provision elegida mediante un evento
@@ -31,16 +48,26 @@ class SearchProvision extends Component
     $this->dispatch('append-provision', id: $id);
   }
 
+  //* buscar suministros NO asociados al proveedor
+  // los mismos deben asociarse con un precio al proveedor
+  public function searchProvisions()
+  {
+    return Provision::whereDoesntHave('suppliers', function ($query) {
+                        $query->where('supplier_id', $this->supplier_id);
+                      })->when($this->search, function ($query) {
+                        $query->where('provision_name', 'like', '%' . $this->search . '%');
+                      })->when($this->search_tr, function ($query) {
+                        $query->where('provision_trademark_id', $this->search_tr);
+                      })->when($this->search_ty, function ($query) {
+                        $query->where('provision_type_id', $this->search_ty);
+                      })->orderBy('id', 'desc')
+                        ->paginate(4);
+  }
+
   public function render()
   {
-    $provisions = Provision::when($this->search, function ($query) {
-      $query->where('provision_name', 'like', '%' . $this->search . '%');
-    })->orderBy('id', 'desc')->paginate(4);
+    $provisions = $this->searchProvisions();
 
-    $provisions_filter = Provision::whereDoesntHave('suppliers', function ($query) {
-      $query->where('supplier_id', $this->supplier_id);
-    })->paginate(4);
-
-    return view('livewire.suppliers.search-provision', compact('provisions_filter'));
+    return view('livewire.suppliers.search-provision', compact('provisions'));
   }
 }
--- resources/views/livewire/suppliers/search-provision.blade.php
+++ resources/views/livewire/suppliers/search-provision.blade.php
@@ -1,79 +1,67 @@
 <div class="w-full">
-  {{-- formulario de busqueda --}}
-  <form class="w-full">
-
-    <div class="flex flex-col w-full">
-
-      {{-- buscar --}}
-      <div class="flex w-full">
-        {{-- termino de busqueda --}}
+  {{-- desplegable --}}
+  <div x-data="{ open: true }">
+    <x-div-toggle title="cuadro de búsqueda" class="px-1">
+      {{-- formulario de busqueda --}}
+      <form class="w-full">
         <div class="flex flex-col w-full">
-          <x-input-label>buscar suministros</x-input-label>
-          <x-text-input wire:model.live="search" name="search" type="text" placeholder="ingrese un id o termino de búsqueda ..." />
-        </div>
-      </div>
-
-      {{-- seleccion de resultado --}}
-      <div class="flex flex-col w-full">
-        {{-- <x-table-base>
-          <x-slot:tablehead>
-            <tr class="border bg-neutral-100">
-              <x-table-th>id</x-table-th>
-              <x-table-th>nombre</x-table-th>
-              <x-table-th>cantidad</x-table-th>
-              <x-table-th>marca</x-table-th>
-              <x-table-th>tipo</x-table-th>
-              <x-table-th>seleccion</x-table-th>
-            </tr>
-          </x-slot:tablehead>
-          <x-slot:tablebody>
+          {{-- buscar --}}
+          <div class="flex w-full gap-1 bg-neutral-100 p-1">
+            {{-- termino de busqueda --}}
+            <div class="flex flex-col w-full justify-end">
+              <x-input-label>buscar suministros</x-input-label>
+              <x-text-input wire:model.live="search" wire:click="resetPagination" name="search" type="text" placeholder="ingrese un id o termino de búsqueda ..." />
+            </div>
+            {{-- filtrar por marca --}}
+            <div class="flex flex-col w-full justify-end">
+              <select wire:model.live="search_tr" wire:click="resetPagination" name="search_tr" id="search_tr" class="p-1 text-sm border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300">
+                <option selected value="">seleccione una marca ...</option>
+                @forelse ($trademarks as $tr)
+                  <option value="{{ $tr->id }}">{{ $tr->provision_trademark_name }}</option>
+                @empty
+                  <option value="">sin opciones ...</option>
+                @endforelse
+              </select>
+            </div>
+            {{-- filtrar por tipo --}}
+            <div class="flex flex-col w-full justify-end">
+              <select wire:model.live="search_ty" wire:click="resetPagination" name="search_ty" id="search_ty" class="p-1 text-sm border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300">
+                <option selected value="">seleccione un tipo ...</option>
+                @forelse ($provision_types as $ty)
+                  <option value="{{ $ty->id }}">{{ $ty->provision_type_name }}</option>
+                @empty
+                  <option value="">sin opciones ...</option>
+                @endforelse
+              </select>
+            </div>
+          </div>
+          {{-- seleccion de resultado --}}
+          <div class="flex flex-col w-full border-t border-neutral-200">
             @forelse ($provisions as $provision)
-              <tr wire:key="{{ $provision->id }}" class="border">
-                <x-table-td>{{ $provision->id }}</x-table-td>
-                <x-table-td>{{ $provision->provision_name }}</x-table-td>
-                <x-table-td>{{ $provision->provision_quantity }}({{ $provision->measure->measure_abrv }})</x-table-td>
-                <x-table-td>{{ $provision->trademark->provision_trademark_name }}</x-table-td>
-                <x-table-td>{{ $provision->type->provision_type_name }}</x-table-td>
-                <x-table-td>
-                  <span wire:click="addProvision({{ $provision->id }})" class="font-bold cursor-pointer text-lg leading-none">&plus;</span>
-                </x-table-td>
-              </tr>
+              <div wire:key="{{ $provision->id }}" class="flex justify-between p-1 border-x border-b">
+                {{-- datos de suministro --}}
+                <div class="flex">
+                  <span class="font-semibold">{{ $provision->type->provision_type_name }}:&nbsp;</span>
+                  <span>{{ $provision->provision_name }}&nbsp;</span>
+                  <span>{{ $provision->provision_quantity }}({{ $provision->measure->measure_abrv }}),&nbsp;</span>
+                  <span class="font-semibold">marca:&nbsp;{{ $provision->trademark->provision_trademark_name }}</span>
+                </div>
+                {{-- botones --}}
+                <div class="flex">
+                  <span wire:click="addProvision({{ $provision->id }})" class="font-bold cursor-pointer text-lg leading-none px-1 mx-1 bg-neutral-100 text-neutral-600 border-neutral-300 rounded-sm" title="agregar a la lista.">&plus;</span>
+                </div>
+              </div>
             @empty
-              <tr class="border">
-                <td colspan="5">sin registros!</td>
-              </tr>
+              <div class="border">
+                <span colspan="5">sin registros!</span>
+              </div>
             @endforelse
-          </x-slot:tablebody>
-        </x-table-base>
-        <div class="flex justify-end items-center gap-4 py-1">
-          {{ $provisions->links() }}
-        </div> --}}
-
-        @forelse ($provisions_filter as $provision)
-          <div wire:key="{{ $provision->id }}" class="flex justify-between p-1 border-x border-b">
-            {{-- datos de suministro --}}
-            <div class="flex">
-              <span class="font-semibold">{{ $provision->type->provision_type_name }}:&nbsp;</span>
-              <span>{{ $provision->provision_name }}&nbsp;</span>
-              <span>{{ $provision->provision_quantity }}({{ $provision->measure->measure_abrv }}),&nbsp;</span>
-              <span class="font-semibold">marca:&nbsp;{{ $provision->trademark->provision_trademark_name }}</span>
-            </div>
-            {{-- botones --}}
-            <div class="flex">
-              <span wire:click="addProvision({{ $provision->id }})" class="font-bold cursor-pointer text-lg leading-none px-1 mx-1 bg-neutral-100 text-neutral-600 border-neutral-300 rounded-sm" title="agregar a la lista.">&plus;</span>
+            <div class="flex justify-end items-center gap-4 py-1">
+              {{ $provisions->links() }}
             </div>
           </div>
-        @empty
-          <div class="border">
-            <span colspan="5">sin registros!</span>
-          </div>
-        @endforelse
-        <div class="flex justify-end items-center gap-4 py-1">
-          {{ $provisions_filter->links() }}
         </div>
-      </div>
-
-    </div>
-
-  </form>
+      </form>
+    </x-div-toggle>
+  </div>
 </div>
