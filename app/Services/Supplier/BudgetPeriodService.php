75ad24ac0d950752066e43bce7c7bcf41e47205f
Osvaldo <osvaldozakowicz@gmail.com>
2024-11-30 19:27:35 -0300

    agrega cambios al modulo de proveedores
    mueve la logica de obtencion de periodos a abrir o cerrar a un servicio

--- app/Services/Supplier/BudgetPeriodService.php
+++ app/Services/Supplier/BudgetPeriodService.php
@@ -3,26 +3,66 @@
 namespace App\Services\Supplier;
 
 use App\Models\PeriodStatus;
+use App\Models\RequestForQuotationPeriod;
 
 class BudgetPeriodService
 {
-  // todo: obtener de la base de datos
-  public $status_scheduled_id = 1;
-  public $status_open_id = 2;
-  public $status_closed_id = 3;
-
+  //* obtener estado programado
   public function getStatusScheduled()
   {
-    return $this->status_scheduled_id;
+    $status_scheduled = PeriodStatus::where('status_name', 'programado')
+      ->where('status_code', 0)->first();
+
+    return $status_scheduled->id;
   }
 
+  //* obtener estado abierto
   public function getStatusOpen()
   {
-    return $this->status_open_id;
+    $status_open = PeriodStatus::where('status_name', 'abierto')
+      ->where('status_code', 1)->first();
+
+    return $status_open->id;
   }
 
+  //* obtener estado cerrado
   public function getStatusClosed()
   {
-    return $this->status_closed_id;
+    $status_close = PeriodStatus::where('status_name', 'abierto')
+      ->where('status_code', 1)->first();
+
+    return $status_close->id;
+  }
+
+  /**
+   * * obtener periodos de peticion de presupuestos que
+   * deben abrirse a la fecha actual.
+  */
+  public function getQuotationPeriodsToOpen()
+  {
+    // fecha actual
+    $today = now()->format('Y-m-d');
+
+    // periodos cuya fecha de inicio sea igual a la fecha actual, y estatus programado
+    return RequestForQuotationPeriod::whereHas('status', function ($query) {
+        $query->where('status_name', 'programado');
+      })->where('period_start_at', $today)
+        ->get();
+  }
+
+  /**
+   * * obtener periodos de peticion de presupuestos que
+   * deben cerrarse a la fecha actual.
+  */
+  public function getQuotationPeriodsToClose()
+  {
+    // fecha actual
+    $today = now()->format('Y-m-d');
+
+    // periodos cuya fecha de fin sea igual a la fecha actual, y estatus abierto
+    return RequestForQuotationPeriod::whereHas('status', function ($query) {
+        $query->where('status_name', 'abierto');
+      })->where('period_end_at', $today)
+        ->get();
   }
 }
--- routes/console.php
+++ routes/console.php
@@ -18,50 +18,43 @@ use Illuminate\Support\Facades\Artisan;
  * ->everyMinute(); ejecutar el comando cada minuto.
  * ->everyTwoMinutes(); ejecutar el comando cada dos minutos.
  * ->everyFiveMinutes(); ejecutar el comando cada cinco minutos.
- */
+*/
+
 
 /**
  * * command: abrir periodos de peticion de presupuesto.
  * @param signature: firma del comando (nombre y argumentos opcionales)
  * @param clusure: callback fn con la logica o servicio del comando.
- * todo: ejecutar cada minuto?
- */
+*/
 Artisan::command('budget-periods:open', function (BudgetPeriodService $bps) {
-  // fecha actual
-  $today = now()->format('Y-m-d');
 
   // periodos cuya fecha de inicio sea igual a la fecha actual, y estatus programado
-  $periods = RequestForQuotationPeriod::whereHas('status', function ($query) {
-    $query->where('status_name', 'programado');
-  })->where('period_start_at', $today)
-    ->get();
+  $periods_to_open = $bps->getQuotationPeriodsToOpen();
 
-  // cada periodo debe abrirse
-  // todo: cada periodo abierto debe contactar a sus proveedores
-  foreach ($periods as $period) {
+  // cada periodo debe abrirse y procesarse
+  foreach ($periods_to_open as $period) {
+
     $period->period_status_id = $bps->getStatusOpen();
     $period->save();
+
   }
 })->purpose('abrir periodos de peticion de presupuestos')->everyMinute();
 
 /**
  * * command: cerrar periodos de peticion de presupuesto.
- * todo: ejecutar cada dos minutos?
+ * @param signature: firma del comando (nombre y argumentos opcionales)
+ * @param clusure: callback fn con la logica o servicio del comando.
  */
 Artisan::command('budget-periods:close', function (BudgetPeriodService $bps) {
-  // fecha actual
-  $today = now()->format('Y-m-d');
 
   // periodos cuya fecha de fin sea igual a la fecha actual, y estatus abierto
-  $periods = RequestForQuotationPeriod::whereHas('status', function ($query) {
-    $query->where('status_name', 'abierto');
-  })->where('period_end_at', $today)
-    ->get();
+  $periods_to_close = $bps->getQuotationPeriodsToClose();
 
-  // cada periodo debe cerrarse
-  // todo: cada periodo abierto debe cerrar, y dejar de contactar con proveedores
-  foreach ($periods as $period) {
+  // cada periodo debe cerrarse y procesarse
+  foreach ($periods_to_close as $period) {
+
     $period->period_status_id = $bps->getStatusClosed();
     $period->save();
+
   }
-})->purpose('cerrar periodos de peticion de presupuestos')->everyTwoMinutes();
+})->purpose('cerrar periodos de peticion de presupuestos')->everyMinute();
