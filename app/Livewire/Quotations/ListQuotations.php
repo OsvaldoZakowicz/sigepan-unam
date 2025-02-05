be7034ccef8cad9e6b08eadc718b94a33e5a7445
Osvaldo <osvaldozakowicz@gmail.com>
2025-02-05 12:52:55 -0300

    agrega cambios al modulo de proveedores
    agrega filtrado de presupuestos recibidos para el proveedor

--- app/Livewire/Quotations/ListQuotations.php
+++ app/Livewire/Quotations/ListQuotations.php
@@ -3,18 +3,108 @@
 namespace App\Livewire\Quotations;
 
 use App\Models\Quotation;
+use App\Models\Supplier;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use Livewire\WithPagination;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class ListQuotations extends Component
 {
   use WithPagination;
 
+  #[Url]
+  public $search_word = '';
+
+  #[Url]
+  public $quotation_status = '';
+
+  #[Url]
+  public $period_status = '';
+
+  #[Url]
+  public $period_start_at = '';
+
+  #[Url]
+  public $period_end_at = '';
+
+  public Supplier $supplier;
+
   /**
-   * todo: vista responsiva
-   */
+   * boot de datos
+   * @return void
+  */
+  public function boot(): void
+  {
+    // cada supplier tiene un usuario, y esta en sesion (Auth::user())
+    $this->supplier = Auth::user()->supplier;
+  }
+
+  /**
+   * reiniciar pagina para reestablecer la paginacion al
+   * buscar periodos.
+   * @return void
+  */
+  public function resetPagination(): void
+  {
+    $this->resetPage();
+  }
+
+  /**
+   * limpiar terminos de busqueda de los inputs
+   * esto tambien reiniciara la paginacion.
+   * @return void
+  */
+  public function resetSearchInputs(): void
+  {
+    $this->reset(['search_word', 'period_status', 'quotation_status', 'period_start_at', 'period_end_at']);
+    $this->resetPagination();
+  }
+
+  /**
+   * buscar presupuestos
+   * @return mixed
+  */
+  public function searchQuotations()
+  {
+    // cada quotation tiene un supplier (proveedor) y un period
+    // obtener la lista de quotations que tiene el proveedor en sesion
+    $quotations = Quotation::with(['supplier', 'period'])
+      ->where('supplier_id', $this->supplier->id)
+      ->when($this->search_word, function ($query) {
+        $query->where('id', $this->search_word)
+            ->orWhere('quotation_code', 'like', '%' . $this->search_word . '%');
+      })
+      ->when(isset($this->quotation_status) && $this->quotation_status !== '', function ($query) {
+        $query->where('is_completed', $this->quotation_status);
+      })
+      ->when(isset($this->period_status) && $this->period_status !== '', function ($query) {
+        $query->whereHas('period', function($q) {
+          $q->where('period_status_id', $this->period_status);
+        });
+      })
+      ->when($this->period_start_at && $this->period_end_at, function ($query) {
+        $query->whereHas('period', function($q) {
+          $q->where('period_start_at', '>=', $this->period_start_at)
+            ->where('period_end_at', '<=', $this->period_end_at);
+        });
+      })
+      ->when($this->period_start_at && !$this->period_end_at, function ($query) {
+        $query->whereHas('period', function($q) {
+          $q->where('period_start_at', '>=', $this->period_start_at);
+        });
+      })
+      ->when(!$this->period_start_at && $this->period_end_at, function ($query) {
+        $query->whereHas('period', function($q) {
+          $q->where('period_end_at', '<=', $this->period_end_at);
+        });
+      })
+      ->orderBy('id', 'desc')
+      ->paginate(10);
+
+    return $quotations;
+  }
 
   /**
    * renderizar vista
@@ -22,15 +112,7 @@ class ListQuotations extends Component
   */
   public function render(): View
   {
-    // cada supplier tiene un usuario, y esta en sesion (Auth::user())
-    $supplier_id = Auth::user()->supplier->id;
-
-    // cada quotation tiene un supplier (proveedor)
-    // obtener la lista de quotations que tiene el proveedor en sesion
-    $quotations = Quotation::with('supplier')
-      ->where('supplier_id', $supplier_id)
-      ->paginate(10);
-
+    $quotations = $this->searchQuotations();
     return view('livewire.quotations.list-quotations', compact('quotations'));
   }
 }
--- resources/views/livewire/quotations/list-quotations.blade.php
+++ resources/views/livewire/quotations/list-quotations.blade.php
@@ -8,9 +8,97 @@
     {{-- cuerpo --}}
     <x-content-section>
 
-      <x-slot:header class="hidden">
-        <!-- grupo de botones -->
-        <div class="flex"></div>
+      <x-slot:header class="">
+
+        {{-- busqueda --}}
+        <div class="flex gap-1 justify-start items-start grow">
+
+          {{-- termino de busqueda --}}
+          <div class="flex flex-col justify-end w-1/6">
+            <label for="search_word">buscar presupuesto</label>
+            <input
+              type="text"
+              name="search_word"
+              id="search_word"
+              wire:model.live="search_word"
+              wire:click="resetPagination()"
+              placeholder="ingrese un id, codigo de presupuesto o de periodo ..."
+              class="w-full text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+            />
+          </div>
+
+          {{-- estado del presupuesto --}}
+          <div class="flex flex-col justify-end w-1/6">
+            <label for="quotation_status">estado del presupuesto</label>
+            <select
+              name="quotation_status"
+              id="quotation_status"
+              wire:model.live="quotation_status"
+              wire:click="resetPagination()"
+              class="w-full text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+              >
+              <option value="">seleccione un estado ...</option>
+              <option value="1">respondido</option>
+              <option value="0">sin responder</option>
+            </select>
+          </div>
+
+          {{-- estado del periodo --}}
+          <div class="flex flex-col justify-end w-1/6">
+            <label for="period_status">estado del periodo</label>
+            <select
+              name="period_status"
+              id="period_status"
+              wire:model.live="period_status"
+              wire:click="resetPagination()"
+              class="w-full text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+              >
+              <option value="">seleccione un estado ...</option>
+              <option value="2">abierto</option>
+              <option value="3">cerrado</option>
+            </select>
+          </div>
+
+          {{-- fecha de inicio --}}
+          <div class="flex flex-col justify-end w-1/6">
+            <label for="period_start_at">fecha de recepción desde</label>
+            <input
+              type="date"
+              name="period_start_at"
+              id="period_start_at"
+              wire:model.live="period_start_at"
+              wire:click="resetPagination()"
+              class="w-full text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+            />
+          </div>
+
+          {{-- fecha de fin --}}
+          <div class="flex flex-col justify-end w-1/6">
+            <label for="period_end_at">fecha de disponibilidad hasta</label>
+            <input
+              type="date"
+              name="period_end_at"
+              id="period_end_at"
+              wire:model.live="period_end_at"
+              wire:click="resetPagination()"
+              class="w-full text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+            />
+          </div>
+
+        </div>
+
+        {{-- limpiar campos de busqueda --}}
+        <div class="flex flex-col self-start h-full">
+          <x-a-button
+            href="#"
+            wire:click="resetSearchInputs()"
+            bg_color="neutral-200"
+            border_color="neutral-300"
+            text_color="neutral-600"
+            >limpiar
+          </x-a-button>
+        </div>
+
       </x-slot:header>
 
       <x-slot:content class="flex-col gap-1">
@@ -25,6 +113,7 @@
               <x-table-th class="text-start">codigo del presupuesto</x-table-th>
               <x-table-th class="text-start">estado del presupuesto</x-table-th>
               <x-table-th class="text-start">periodo</x-table-th>
+              <x-table-th class="text-start">recibido el</x-table-th>
               <x-table-th class="text-start">disponible hasta</x-table-th>
               <x-table-th class="text-start w-60">acciones</x-table-th>
             </tr>
@@ -47,46 +136,38 @@
                   @endif
                 </x-table-td>
                 <x-table-td class="text-start">
-                  <span>
-                    {{ $quotation->period->period_code }}
-                  </span>
-                  <span>,&nbsp;el periodo está:&nbsp;</span>
+                  <span>{{ $quotation->period->period_code }},&nbsp;el periodo está:&nbsp;</span>
                   {{-- estado del periodo --}}
-                  <span>
-                    @switch($quotation->period->status->status_code)
-
-                      @case(1)
-                        {{-- abierto --}}
-                        <span
-                          class="font-semibold text-emerald-600 cursor-pointer"
-                          title="{{ $quotation->period->status->status_short_description }}"
-                          >{{ $quotation->period->status->status_name }}
-                        </span>
-                        @break
-
-                      @default
-                        {{-- cerrado --}}
-                        <span
-                          class="font-semibold text-red-400 cursor-pointer"
-                          title="{{ $quotation->period->status->status_short_description }}"
-                          >{{ $quotation->period->status->status_name }}
-                        </span>
-
-                    @endswitch
-                  </span>
-
+                  @if ($quotation->period->status->status_code == 1)
+                    {{-- abierto --}}
+                    <span
+                      class="font-semibold text-emerald-600 cursor-pointer"
+                      title="{{ $quotation->period->status->status_short_description }}"
+                      >{{ $quotation->period->status->status_name }}
+                    </span>
+                  @else
+                    {{-- cerrado --}}
+                    <span
+                      class="font-semibold text-red-400 cursor-pointer"
+                      title="{{ $quotation->period->status->status_short_description }}"
+                      >{{ $quotation->period->status->status_name }}
+                    </span>
+                  @endif
+                </x-table-td>
+                <x-table-td class="text-start">
+                  {{ formatDateTime($quotation->period->period_start_at, 'd-m-Y') }}
                 </x-table-td>
                 <x-table-td class="text-start">
                   {{ formatDateTime($quotation->period->period_end_at, 'd-m-Y') }}
                 </x-table-td>
                 <x-table-td>
                   <div class="flex justify-start gap-1">
-
-                    {{-- no puedo responder si esta cerrado el periodo --}}
+                    {{-- si el periodo NO esta cerrado --}}
                     @if ($quotation->period->period_status_id !== 3)
 
                       {{-- responder si no esta completado, de lo contrario, editar --}}
                       @if ($quotation->is_completed)
+
                         <x-a-button
                           wire:navigate
                           href="#"
@@ -95,7 +176,9 @@
                           text_color="neutral-600"
                           >modificar
                         </x-a-button>
+
                       @else
+
                         <x-a-button
                           wire:navigate
                           href="{{ route('quotations-quotations-respond', $quotation->id) }}"
@@ -104,15 +187,27 @@
                           text_color="neutral-600"
                           >responder
                         </x-a-button>
-                      @endif
-                    @endif
 
+                      @endif
+
+                    @else
+
+                      <x-a-button
+                        wire:navigate
+                        href="#"
+                        bg_color="neutral-100"
+                        border_color="neutral-200"
+                        text_color="neutral-600"
+                        >ver
+                      </x-a-button>
+
+                    @endif
                   </div>
                 </x-table-td>
               </tr>
             @empty
               <tr class="border">
-                <td colspan="8">¡sin registros! - aún no ha recibido pedidos de presupuesto.</td>
+                <td colspan="7">¡sin registros! - aún no ha recibido pedidos de presupuesto.</td>
               </tr>
             @endforelse
           </x-slot:tablebody>
