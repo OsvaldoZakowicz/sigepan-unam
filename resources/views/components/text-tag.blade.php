bfb7a208bedc5ed0cb545562bfbadc2a9e765fd1
Osvaldo <osvaldozakowicz@gmail.com>
2025-06-18 21:44:26 -0300

    agrega cambios generales
    corrige componente text tag para definir explicitamente las variantes de color

--- resources/views/components/text-tag.blade.php
+++ resources/views/components/text-tag.blade.php
@@ -1,11 +1,18 @@
 @props([
-  'color' => 'neutral'
+'color' => 'neutral'
 ])
 
-<span {{ $attributes->merge(
-    [
-      'class' => 'bg-'.$color.'-200 text-'.$color.'-800 text-sm font-normal px-2.5 py-0.5 rounded-md'
-    ]
-  ) }}>
+@php
+$colorClasses = [
+'neutral' => 'bg-neutral-200 text-neutral-800',
+'emerald' => 'bg-emerald-200 text-emerald-800',
+'red' => 'bg-red-200 text-red-800',
+];
+$classes = $colorClasses[$color] ?? $colorClasses['neutral'];
+@endphp
+
+<span {{ $attributes->merge([
+  'class' => $classes . ' text-sm font-normal px-2.5 py-0.5 rounded-md'
+  ]) }}>
   {{ $slot ?? 'text' }}
 </span>
