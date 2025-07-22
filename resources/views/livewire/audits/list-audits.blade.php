db95fad87c540db6922ab877dd40694855695bee
osvaldo emanuel <osvaldozakowicz@gmail.com>
2025-07-22 14:30:11 -0300

    agrega cambios al modulo de auditoria
    correcciones visuales a la busqueda y lista de registros de auditoria

--- resources/views/livewire/audits/list-audits.blade.php
+++ resources/views/livewire/audits/list-audits.blade.php
@@ -7,12 +7,10 @@
     <x-content-section>
 
       <x-slot:header>
-
         {{-- campos de busqueda --}}
-        <div class="flex gap-1 justify-start items-start grow">
-
+        <div class="flex items-end justify-start gap-1">
           {{-- termino de busqueda --}}
-          <div class="flex flex-col justify-end w-1/6">
+          <div class="flex flex-col justify-end w-56">
             <label for="">buscar id</label>
             <input
               type="text"
@@ -21,19 +19,18 @@
               wire:model.live="search"
               wire:click="resetPagination()"
               placeholder="ingrese un id"
-              class="shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+              class="p-1 text-sm border shrink border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
             />
           </div>
-
           {{-- evento --}}
-          <div class="flex flex-col justify-end w-1/6">
+          <div class="flex flex-col justify-end w-56">
             <label for="">filtrar por evento</label>
             <select
               wire:model.live="event"
               wire:click="resetPagination()"
               name="event"
               id="event"
-              class="shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+              class="p-1 text-sm border shrink border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
             >
               <option value="" selected>seleccione ...</option>
               <option value="created">creado</option>
@@ -41,27 +38,24 @@
               <option value="deleted">borrado</option>
             </select>
           </div>
-
           {{-- tabla --}}
-          <div class="flex flex-col justify-end w-1/6">
+          <div class="flex flex-col justify-end w-56">
             <label for="">filtrar por tabla</label>
             <select
               wire:model.live="table"
               wire:click="resetPagination()"
               name="table"
               id="table"
-              class="shrink text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
+              class="p-1 text-sm border shrink border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"
               >
               <option value="" selected>seleccione ...</option>
-              {{-- iterar el array $tables, por cada $table generar una etiqueta option --}}
               @foreach ($tables as $table)
                 <option value="{{ $table }}">{{ __( $table ) }}</option>
               @endforeach
             </select>
           </div>
-
           {{-- fecha de inicio --}}
-          <div class="flex flex-col justify-end w-1/6">
+          <div class="flex flex-col justify-end w-56">
             <label for="search_start_at">eventos desde</label>
             <input
               type="date"
@@ -69,11 +63,10 @@
               id="search_start_at"
               wire:model.live="search_start_at"
               wire:click="resetPagination()"
-              class="w-full text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"/>
+              class="w-full p-1 text-sm border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"/>
           </div>
-
           {{-- fecha de fin --}}
-          <div class="flex flex-col justify-end w-1/6">
+          <div class="flex flex-col justify-end w-56">
             <label for="search_end_at">eventos hasta</label>
             <input
               type="date"
@@ -81,22 +74,18 @@
               id="search_end_at"
               wire:model.live="search_end_at"
               wire:click="resetPagination()"
-              class="w-full text-sm p-1 border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"/>
+              class="w-full p-1 text-sm border border-neutral-200 focus:outline-none focus:ring focus:ring-neutral-300"/>
           </div>
-        </div>
-
-        {{-- limpiar campos de busqueda --}}
-        <div class="flex flex-col self-start h-full">
+          {{-- limpiar campos de busqueda --}}
           <x-a-button
             href="#"
             wire:click="resetSearchInputs()"
             bg_color="neutral-200"
             border_color="neutral-300"
             text_color="neutral-600"
-            >limpiar
+            >limpiar filtros
           </x-a-button>
         </div>
-
       </x-slot:header>
 
       <x-slot:content>
@@ -104,17 +93,28 @@
         <x-table-base>
           <x-slot:tablehead>
             <tr class="border bg-neutral-100">
-              <x-table-th class="text-end w-12">id</x-table-th>
-              <x-table-th class="text-start">evento</x-table-th>
-              <x-table-th class="text-start">tabla afectada</x-table-th>
-              <x-table-th class="text-start">id del registro</x-table-th>
-              <x-table-th
-                class="text-start cursor-pointer"
-                title="usuario o proveedor que generó el evento sobre el registro"
-                >responsable&nbsp;<x-quest-icon/>
+              <x-table-th class="w-12 text-end">
+                id
               </x-table-th>
-              <x-table-th class="text-end">fecha del evento</x-table-th>
-              <x-table-th class="text-start w-48">acciones</x-table-th>
+              <x-table-th class="text-start">
+                evento
+              </x-table-th>
+              <x-table-th class="text-start">
+                tabla afectada
+              </x-table-th>
+              <x-table-th class="text-start">
+                id del registro
+              </x-table-th>
+              <x-table-th
+                class="cursor-pointer text-start"
+                >responsable&nbsp;<x-quest-icon title="usuario o proveedor que generó el evento sobre el registro"/>
+              </x-table-th>
+              <x-table-th class="text-end">
+                fecha del evento
+              </x-table-th>
+              <x-table-th class="w-48 text-start">
+                acciones
+              </x-table-th>
             </tr>
           </x-slot:tablehead>
           <x-slot:tablebody>
@@ -139,7 +139,7 @@
                   {{ formatDateTime($audit->created_at)}}
                 </x-table-td>
                 <x-table-td class="text-start">
-                  <div class="w-fit-content inline-flex gap-2 justify-start items-center">
+                  <div class="inline-flex items-center justify-start gap-2 w-fit-content">
 
                     <x-a-button
                       wire:navigate
@@ -164,7 +164,7 @@
             </tr>
             @empty
             <tr class="border">
-              <td colspan="7">sin registros!</td>
+              <td colspan="7">¡sin registros!</td>
             </tr>
             @endforelse
           </x-slot:tablebody>
